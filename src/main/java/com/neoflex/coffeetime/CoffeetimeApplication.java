e0b1ee5d7adca9c9e5bf58ac05ae80c82c2bcbe7
alexcypriiz <alexey.podlubny@yandex.ru>
2021-10-20 22:33:08 +0400

    init commit

---
+++ src/main/java/com/neoflex/coffeetime/CoffeetimeApplication.java
@@ -0,0 +1,13 @@
+package com.neoflex.coffeetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeetimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoflex/coffeetime/CoffeetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoflex.coffeetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeetimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
